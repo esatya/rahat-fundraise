1441c3249b3142d8e86e2a92b88d77e8a432a2b0
iheathers <shresthapritam7@gmail.com>
2022-05-12 20:36:44 +0545

    Integrate campaign and user

--- frontend/src/main-component/ProjectPage/index.js
+++ frontend/src/main-component/ProjectPage/index.js
@@ -1,44 +1,57 @@
-import React, {Fragment} from 'react';
-import Navbar from '../../components/Navbar'
-import PageTitle from '../../components/pagetitle'
-import Scrollbar from '../../components/scrollbar'
-import Causes from '../../api/cause'
-import CauseTabs from './alltab'
-import CauseSidebar from './sidebar'
-import Logo from '../../images/logo.png'
-import Footer from '../../components/footer';
+import React, { Fragment, useEffect } from "react";
+import Navbar from "../../components/Navbar";
+import PageTitle from "../../components/pagetitle";
+import Scrollbar from "../../components/scrollbar";
+import Causes from "../../api/cause";
+import CauseTabs from "./alltab";
+import CauseSidebar from "./sidebar";
+import Logo from "../../images/logo.png";
+import Footer from "../../components/footer";
+import { toast } from "react-toastify";
 
+const CauseSinglePage = (props) => {
+  const [campaign, setCampaign] = React.useState({});
 
+  const id = props.match.params.id;
 
-const CauseSinglePage =(props) => {
+  console.log({ id });
 
-    const id = props.match.params.id;
+  useEffect(async () => {
+    try {
+      const resData = await fetch(
+        `http://localhost:8080/api/campaign/get-by-id/${id}`
+      ).then((res) => res.json());
 
-    const CauseDetails = Causes.find( item => item.id === id)
+      console.log({ resData });
 
+      setCampaign(resData.data);
+    } catch (error) {
+      return toast.error(error.message);
+    }
+  }, []);
 
-    return(
-        <Fragment>
-            <Navbar Logo={Logo}/>
-            <PageTitle pageTitle={CauseDetails.cTitle} pagesub={'Case Single'}/> 
-            <div className="wpo-case-details-area section-padding">
-                <div className="container">
-                    <div className="row">
-                        <div className="col col-lg-8">
-                            <div className="wpo-case-details-wrap">
-                                <div className="wpo-case-details-img">
-                                    <img src={CauseDetails.cImgSingle} alt=""/>
-                                </div>
-                                 <CauseTabs/>
-                            </div>
-                        </div>
-                        <CauseSidebar/>
-                    </div>
+  return (
+    <Fragment>
+      <Navbar Logo={Logo} />
+      <PageTitle pageTitle={campaign.title} pagesub={"Case Single"} />
+      <div className="wpo-case-details-area section-padding">
+        <div className="container">
+          <div className="row">
+            <div className="col col-lg-8">
+              <div className="wpo-case-details-wrap">
+                <div className="wpo-case-details-img">
+                  <img src={Causes[0].cImgSingle} alt="" />
                 </div>
+                <CauseTabs campaign={campaign} />
+              </div>
             </div>
-            <Footer/>
-            <Scrollbar/>
-        </Fragment>
-    )
+            <CauseSidebar />
+          </div>
+        </div>
+      </div>
+      <Footer />
+      <Scrollbar />
+    </Fragment>
+  );
 };
 export default CauseSinglePage;
