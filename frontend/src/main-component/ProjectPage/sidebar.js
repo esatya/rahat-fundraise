79a1ee61d1a55a34d36c8fff2387793bcaf6b1d6
mani byanjankar <mani.byanjankar@rumsan.com>
2022-05-22 19:57:55 +0545

    multi step form updated and register here page designed

--- frontend/src/main-component/ProjectPage/sidebar.js
+++ frontend/src/main-component/ProjectPage/sidebar.js
@@ -2,13 +2,18 @@ import React, { useState } from "react";
 import StepZilla from "react-stepzilla";
 
 import "../../sass/multistep.css";
+import "./style.css";
 import Step1 from "../../components/MultistepForm/step1";
 import Step2 from "../../components/MultistepForm/step2";
 import Step3 from "../../components/MultistepForm/step3";
 
 const CauseSidebar = (props) => {
   const [sampleStore, setSampleStore] = useState({});
-
+  const [donation, setDonations] = useState({
+    toggle: false,
+    amount: "",
+    project_name: "",
+  });
   const getStore = () => {
     return sampleStore;
   };
@@ -17,6 +22,10 @@ const CauseSidebar = (props) => {
     setSampleStore((prev) => ({ ...prev, ...update }));
   };
 
+  const onChange = (e) => {
+    const { amount, project_name } = e;
+    setDonations({ toggle: true, amount, project_name });
+  };
   const steps = [
     {
       name: "Pledge",
@@ -41,21 +50,46 @@ const CauseSidebar = (props) => {
           campaign={props.campaign}
           donated={props.donated}
           setDonated={props.setDonated}
+          onChange={(e) => onChange(e)}
         />
       ),
     },
   ];
-
+  console.log(props);
   return (
-    <div className="col col-lg-4 col-12">
-      <div className="blog-sidebar">
-        <div className="widget category-widget">
-          <h3>Donate Here</h3>
-          <div className="step-progress">
-            <StepZilla steps={steps} />
+    <div className="col col-lg-5 col-12 ">
+      {donation.toggle ? (
+        <div className="blog-sidebar">
+          <div className="widget category-widget">
+            <h3>Registered</h3>
+            <div
+              className="step-progress custom-step-progress"
+              style={{ width: "340px" }}
+            >
+              <div>
+                <div>{donation.amount}</div>
+                <div>{donation.project_name}</div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className="blog-sidebar">
+          <div className="widget category-widget">
+            <h3>Donate Here</h3>
+            <div
+              className="step-progress custom-step-progress"
+              style={{ width: "340px" }}
+            >
+              <StepZilla
+                steps={steps}
+                backButtonCls="btn btn-lg pull-left custom-color"
+                nextButtonCls="btn btn-primary btn-lg pull-right"
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
