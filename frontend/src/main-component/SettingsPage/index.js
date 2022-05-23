20165c42ce3d66eaebb13a18c2726fd16a2bce7c
Divyaswor Makai <makaidivya@gmail.com>
2022-05-23 20:29:03 +0545

    update: minor grammar fix

--- frontend/src/main-component/SettingsPage/index.js
+++ frontend/src/main-component/SettingsPage/index.js
@@ -1,17 +1,20 @@
-import React, { Fragment } from "react";
-import Navbar from "../../components/Navbar";
-import PageTitle from "../../components/pagetitle";
-import Footer from "../../components/footer";
-import Scrollbar from "../../components/scrollbar";
-import Logo from "../../images/logo.png";
-import SettingSection from "../../components/SettingSection";
+import React, { Fragment } from 'react';
+import Navbar from '../../components/Navbar';
+import PageTitle from '../../components/pagetitle';
+import Footer from '../../components/footer';
+import Scrollbar from '../../components/scrollbar';
+import Logo from '../../images/logo.png';
+import SettingSection from '../../components/SettingSection';
 
 const SettingPage = () => {
   return (
     <Fragment>
       <Navbar Logo={Logo} />
-      <PageTitle pageTitle={"Settings Page"} pagesub={"Settings /Account"} />
-      <SettingSection evCLass={"section-padding"} />
+      <PageTitle
+        pageTitle={'Profile Settings'}
+        pagesub={'Settings / Account'}
+      />
+      <SettingSection evCLass={'section-padding'} />
       <Footer />
       <Scrollbar />
     </Fragment>
