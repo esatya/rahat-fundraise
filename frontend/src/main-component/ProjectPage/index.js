60df03b1457a8b97435fe08be34079ef611715a1
Divyaswor Makai <makaidivya@gmail.com>
2022-05-25 22:42:49 +0545

    Share button

--- frontend/src/main-component/ProjectPage/index.js
+++ frontend/src/main-component/ProjectPage/index.js
@@ -1,26 +1,5 @@
 import { toast } from 'react-toastify';
 import React, { Fragment, useContext, useEffect, useState } from 'react';
-import {
-  FacebookShareButton,
-  RedditShareButton,
-  TwitterShareButton,
-  LinkedinShareButton,
-  TelegramShareButton,
-  ViberShareButton,
-  FacebookMessengerShareButton,
-  EmailShareButton,
-  EmailIcon,
-} from 'react-share';
-import {
-  FacebookIcon,
-  FacebookMessengerIcon,
-  LinkedinIcon,
-  RedditIcon,
-  TelegramIcon,
-  TwitterIcon,
-  ViberIcon,
-  WhatsappIcon,
-} from 'react-share';
 
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/footer';
@@ -65,36 +44,6 @@ const CauseSinglePage = (props) => {
           <div className="row">
             <div className="col col-lg-7">
               <div className="wpo-case-details-wrap">
-                <div className="d-flex flex-row align-items-center justify-content-end">
-                  <FacebookShareButton url={window.location.href}>
-                    <FacebookIcon size={32} round={true} className="mx-1" />
-                  </FacebookShareButton>
-                  <RedditShareButton url={window.location.href}>
-                    <RedditIcon size={32} round={true} className="mx-1" />
-                  </RedditShareButton>
-                  <TwitterShareButton url={window.location.href}>
-                    <TwitterIcon size={32} round={true} className="mx-1" />
-                  </TwitterShareButton>
-                  <LinkedinShareButton url={window.location.href}>
-                    <LinkedinIcon size={32} round={true} className="mx-1" />
-                  </LinkedinShareButton>
-                  <TelegramShareButton url={window.location.href}>
-                    <TelegramIcon size={32} round={true} className="mx-1" />
-                  </TelegramShareButton>
-                  <ViberShareButton url={window.location.href}>
-                    <ViberIcon size={32} round={true} className="mx-1" />
-                  </ViberShareButton>
-                  <FacebookMessengerShareButton url={window.location.href}>
-                    <FacebookMessengerIcon
-                      size={32}
-                      round={true}
-                      className="mx-1"
-                    />
-                  </FacebookMessengerShareButton>
-                  <EmailShareButton url={window.location.href}>
-                    <EmailIcon size={32} round={true} className="mx-1" />
-                  </EmailShareButton>
-                </div>
                 {campaign?.creator === user?.data?.id && (
                   <Link to={`/fundraise/${id}/edit`} className="theme-btn mb-4">
                     Edit
