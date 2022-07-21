8012451d640cf53c377a238595a405a480d15509
Divyaswor Makai <makaidivya@gmail.com>
2022-07-21 22:02:22 +0545

    update: bug fixes and additions of some things

--- frontend/src/components/pagetitle/index.js
+++ frontend/src/components/pagetitle/index.js
@@ -7,14 +7,14 @@ const PageTitle = (props) => {
       <div className="container">
         <div className="row">
           <div className="col-12">
-            <div className="wpo-breadcumb-wrap">
+            <div className="wpo-breadcumb-wrap py-4">
               <h2>{props.pageTitle}</h2>
               <ul>
                 <li>
                   <Link to="/">Home</Link>
                 </li>
                 <li>
-                  <span className="ps-1">{props.pagesub}</span>
+                  <span className="ps-1">{props.pagesub?.slice(0, 20)}</span>
                 </li>
               </ul>
             </div>
